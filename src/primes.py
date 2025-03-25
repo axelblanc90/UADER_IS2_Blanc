26380a21e90f1ac3efb4a2e6b7c662ec50f6f9b4
axelblanc90 <141861011+axelblanc90@users.noreply.github.com>
2025-03-24 21:31:56 -0300

    añadirdo de expicacion de el codigo

--- src/primes.py
+++ src/primes.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
 # Python program to display all the prime numbers within an interval
 
+#en esta parte delimita un maximo y minimo de numeros
 lower = 1
 upper = 500
 
+
 print("Prime numbers between", lower, "and", upper, "are:")
 
-for num in range(lower, upper + 1):
+
+ for num in range(lower, upper + 1):
    # all prime numbers are greater than 1
    if num > 1:
        for i in range(2, num):
+        #si el numero es dibvisible por cualquier numero de i,no es un num primo
            if (num % i) == 0:
                break
        else:
