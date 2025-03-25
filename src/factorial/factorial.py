d27fbe75b7a0d64ed6e60b8bc4240024b7518fbb
axelblanc90 <141861011+axelblanc90@users.noreply.github.com>
2025-03-24 23:06:36 -0300

    cambiado a clase

--- src/factorial/factorial.py
+++ src/factorial/factorial.py
@@ -7,16 +7,21 @@
 #*-------------------------------------------------------------------------*
 import sys
 
-#Verifica si se pasa un argumento, si no, solicita el número
-if len(sys.argv) < 2:
-    number = int(input("Por favor ingresa un número: "))
-else:
-    number = int(sys.argv[1])
-
 def factorial(n):
     if n == 0 or n == 1:
         return 1
     else:
         return n * factorial(n - 1)
 
-print(f"El factorial de {number} es {factorial(number)}")
+#Si el rango no es pasado como argumento, se solicita al usuario
+if len(sys.argv) < 2:
+    range_input = input("Por favor ingresa el rango (por ejemplo, 5-20): ")
+else:
+    range_input = sys.argv[1]
+
+#Separar los números desde-hasta
+start, end = map(int, range_input.split('-'))
+
+#Calcular y mostrar el factorial para cada número en el rango
+for num in range(start, end + 1):
+    print(f"El factorial de {num} es {factorial(num)}")
